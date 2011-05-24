bd9cfc81876d9ece2b3eeedde09c8eec70868982
Kurt Ruppel <kurt.ruppel@gmail.com>
2011-05-24 08:10:03 -0700

    Testing new layout.

--- routes/all.js
+++ routes/all.js
@@ -128,4 +128,8 @@ module.exports = function(app) {
                                             });
                          });
   });
+
+  app.get('/test', function(req, res) {
+    res.render('test');
+  });
 };
